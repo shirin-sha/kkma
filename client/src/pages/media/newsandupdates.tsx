7584912c5364f5184f5cbdbc6106f61fc6abf589
shirin-sha <project.manager4565@gmail.com>
2025-10-06 17:49:31 +0400

    latest

--- client/src/pages/media/newsandupdates.tsx
+++ client/src/pages/media/newsandupdates.tsx
@@ -13,7 +13,117 @@ type Post = {
 }
 
 export default function NewsAndUpdates(): React.JSX.Element {
- 
+  // const fallbackPosts: Post[] = [
+  //   {
+  //     title: 'കെ.കെ.എം.എ മുലാഖാത് 2025',
+  //     href: 'https://kkma.net/mulaqat-2025/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/09/MULAQAT-3-370x270.jpg',
+  //     date: { day: '10', monthYear: "Sep’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'കെ.കെ.എം എ. വയനാട് ജില്ലാ വിദ്യാഭ്യാസ അവാർഡ് വിതരണം ചെയ്തു.',
+  //     href: 'https://kkma.net/wayanad-education-award-2025/',
+  //     img: undefined,
+  //     date: { day: '07', monthYear: "Sep’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'ഇഷ്‌ഖേ റസൂൽ 2025: പ്രവാചക സ്നേഹത്തിന്റെ നിറവിൽ ഒരു സായാഹ്നം.',
+  //     href: 'https://kkma.net/ishqe-rasool-2025-2/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/09/Ishqe-rqsool-25-1-370x270.jpg',
+  //     date: { day: '31', monthYear: "Aug’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'മുനീർ കോടി സാഹിബ്‌  പ്രസ്ഥാനംവീഥിയിലെ കർമ്മയോഗി :  കെ കെ. എം എ',
+  //     href: 'https://kkma.net/muneer-kodi-sahib/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/08/Muneer-kodi-370x270.jpg',
+  //     date: { day: '27', monthYear: "Aug’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'തൃശൂർ ജില്ലാ വിദ്യാർത്ഥികൾക്കൾക്കായി കെ.കെ.എം.എ വിദ്യാഭാസ അവാർഡ് ദാനവും അനുമോദനവും സംഘടിപ്പിച്ചു.',
+  //     href: 'https://kkma.net/trissur-edu-award-2025/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/08/trissur-student-award-1-370x270.jpg',
+  //     date: { day: '27', monthYear: "Aug’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'കെ കെ എം എ ഇഷ്‌ഖേ റസൂൽ ഓഗസ്റ്റ് 29 ന്',
+  //     href: 'https://kkma.net/ishqe-rasool-2025/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/08/Ishq-rasool-1-370x270.jpg',
+  //     date: { day: '23', monthYear: "Aug’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'കെ കെ എം എ യൂത്ത് വിംഗ് നിലവിൽ വന്നു',
+  //     href: 'https://kkma.net/youthwing/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/08/kkma-youth-wing-370x270.jpg',
+  //     date: { day: '20', monthYear: "Aug’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'കെ. കെ. എം. എ  വിദ്യാർത്ഥികൾക്ക് അനുമോദനവും അവാർഡ് ദാനവും സംഘടിപ്പിച്ചു.',
+  //     href: 'https://kkma.net/students-awards-2025/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/08/kkma-student-award-2025-370x270.jpg',
+  //     date: { day: '18', monthYear: "Aug’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'കുവൈത്തിൽ മാനുഷിക സേവനത്തിന്റെ പ്രതീകമായ മാഗ്നെറ്റിനു ആദരവ്.',
+  //     href: 'https://kkma.net/magnet-humanitarian-service/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/08/KKMA-Magnet-2025-1-370x270.jpg',
+  //     date: { day: '10', monthYear: "Aug’25" },
+  //     category: 'All News & Updates',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'കെ.കെ.എം.എ. ‘മർഹബ യാ ശഹ്റു റമദാൻ’ മത പ്രഭാഷണം സംഘടിപ്പിച്ചു.',
+  //     href: 'https://kkma.net/marhaban-ya-shahru-ramadan/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/02/kkma2-370x270.jpeg',
+  //     date: { day: '16', monthYear: "Feb’25" },
+  //     category: 'All News & Updates, KKMA UPDATES, News Kuwait',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'പ്രവാസിയും മാനസിക പ്രശ്നങ്ങളും കെ. കെ. എം. എ. വെബിനാർ സംഘടിപ്പിച്ചു',
+  //     href: 'https://kkma.net/kkma-webinar-2025/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/02/WhatsApp-Image-2025-02-03-at-11.28.52-AM-370x270.jpeg',
+  //     date: { day: '04', monthYear: "Feb’25" },
+  //     category: 'All News & Updates, KKMA UPDATES',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  //   {
+  //     title: 'കെ.കെ.എം.എ. വിദ്യാഭാസ സ്കോളർഷിപ്പ് 2025 വിതരണം ചെയ്തു.',
+  //     href: 'https://kkma.net/education-scholorship-2025/',
+  //     img: 'https://kkma.net/wp-content/uploads/2025/01/WhatsApp-Image-2025-01-29-at-9.18.33-AM-1-370x270.jpeg',
+  //     date: { day: '30', monthYear: "Jan’25" },
+  //     category: 'All News & Updates, KKMA UPDATES',
+  //     author: 'kadmin',
+  //     comments: 0,
+  //   },
+  // ]
+
   const [posts, setPosts] = useState<Post[]>([])
   const [error, setError] = useState('')
   const baseUrl = useMemo(() => (import.meta as any).env?.VITE_API_URL || 'http://localhost:4001', [])
